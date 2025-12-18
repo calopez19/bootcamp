9a96b62bff6fa8f1948683b7ef4b7d155f109437
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-17 23:55:22 -0300

    feat: enhance timer functionality with start, stop, and reset controls

--- react/01-temporizador/src/App.jsx
+++ react/01-temporizador/src/App.jsx
@@ -1,22 +1,68 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [time, setTime] = useState(0)
-  useEffect (() => {
-    if (time) {
-      
+  const [start, setstart] = useState(false);
+  const [time, setTime] = useState(0);
+  const [funcion, setFuncion] = useState('temporizador')
+  useEffect(() => {
+    let moreTime;
+    if (start) {
+      if (funcion === 'temporizador') {
+        moreTime = setTimeout(() => {
+          setTime((prevTime) => prevTime + 1);
+        }, 1000);
+        
+      } else {
+        moreTime = setTimeout(() => {
+          setTime((prevTime) => prevTime + 1);
+        }, 1000);
+        
+      }
     }
 
+    return () => {
+      clearTimeout(moreTime);
+    };
+  }, [time, start]);
 
-  },[time])
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let valor = event.target.algo.value;
+    console.log(valor);
+    const valorNum = parseInt(valor)
+    console.log(valorNum);
+    
+    if (!isNaN(valorNum)) {
+      console.log('paso');
+      event.target.algo.value = ''
+    }
+  };
   return (
     <>
-    
+      <h1>{time}</h1>
+      <button
+        disabled={start}
+        onClick={() => {
+          setstart(true);
+        }}
+      >
+        empezar temporizador
+      </button>
+      <button onClick={() => setstart(false)}>Detener</button>
+      <button
+        onClick={() => {
+          setTime(0);
+        }}
+      >
+        reset temporizador
+      </button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="algo" maxLength={4}/>
+        <button type="submit">subir</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
